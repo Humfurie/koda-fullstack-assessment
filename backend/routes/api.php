2e3dd8963d6ee4c68191cd07e8dbb74ac1757edd
Humphrey Singculan <humfurie@gmail.com>
2026-08-08 12:44:01 +0800

    chore: switch from sanctum to passport

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::apiResource('projects', \App\Http\Controllers\Api\ProjectController::class);
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+
+    Route::apiResource('projects', ProjectController::class);
+});
