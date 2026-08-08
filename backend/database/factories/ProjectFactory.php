655570a6ec198d69d1b9add52b975bafe5c1eef5
Humphrey Singculan <humfurie@gmail.com>
2026-08-08 14:09:44 +0800

    chore: update project factory to be 4 months gap

--- backend/database/factories/ProjectFactory.php
+++ backend/database/factories/ProjectFactory.php
@@ -17,14 +17,16 @@ class ProjectFactory extends Factory
      */
     public function definition(): array
     {
+        $startDate = $this->faker->dateTimeBetween('-3 months', '+1 month');
+
         return [
             'client_name' => $this->faker->company(),
             'project_name' => $this->faker->catchPhrase(),
             'description' => $this->faker->paragraph(),
             'status' => $this->faker->randomElement(['planning', 'in_progress', 'on_hold', 'completed']),
             'priority' => $this->faker->randomElement(['low', 'medium', 'high']),
-            'start_date' => $this->faker->date(),
-            'due_date' => $this->faker->date(),
+            'start_date' => $startDate,
+            'due_date' => $this->faker->dateTimeBetween($startDate, '+4 months'),
         ];
     }
 }
