98867ccd6f1c71245969a673cfceefd5e2df4de8
Humphrey Singculan <humfurie@gmail.com>
2026-08-08 11:46:25 +0800

    feat: update model and api route - add fillable and casts to project model

--- backend/app/Models/Project.php
+++ backend/app/Models/Project.php
@@ -9,4 +9,21 @@ class Project extends Model
 {
     /** @use HasFactory<\Database\Factories\ProjectFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'client_name',
+        'project_name',
+        'description',
+        'status',
+        'priority',
+        'start_date',
+        'due_date',
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
+        'due_date' => 'date',
+    ];
+
+
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('projects', \App\Http\Controllers\Api\ProjectController::class);
